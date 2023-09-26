e0dc0375a5f615a4a498b673eaa07b2cd885da25
Tatyana Akulich <t.n.akulich@gmail.com>
2023-09-26 22:18:27 +0300

    Updated task5a

--- task5a/src/main/java/org/example/FileSearch.java
+++ task5a/src/main/java/org/example/FileSearch.java
@@ -123,6 +123,33 @@ public class FileSearch {
             return super.visitFile(path, basicFileAttributes);
         }
     }
+
+    private static class FileInfo implements Comparable<FileInfo>{
+        String filename;
+        FileTime creationTime;
+
+        public FileInfo(String filename, FileTime creationTime) {
+            this.filename = filename;
+            this.creationTime = creationTime;
+        }
+
+        public String getFilename() {
+            return filename;
+        }
+
+        public FileTime getCreationTime() {
+            return creationTime;
+        }
+
+        @Override
+        public int compareTo(FileInfo o) {
+            if (this.creationTime.toMillis() < o.creationTime.toMillis()) {
+                return 1;
+            } else if (this.creationTime.toMillis() == o.creationTime.toMillis()) {
+                return 0;
+            } else return -1;
+        }
+    }
 }
 
 
