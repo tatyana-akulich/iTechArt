7959005a8219b9709c3dc3a136c52c38eca1fcf5
Tatyana Akulich <t.n.akulich@gmail.com>
2023-10-18 19:36:52 +0300

    updated task 6 using onDialog

--- task6/src/test/java/ProfileTest.java
+++ task6/src/test/java/ProfileTest.java
@@ -4,7 +4,6 @@ import by.itechart.page.*;
 import by.itechart.util.RandomGenerator;
 import com.google.gson.Gson;
 import com.microsoft.playwright.APIResponse;
-import com.microsoft.playwright.Dialog;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -23,6 +22,7 @@ public class ProfileTest extends BaseTest {
     BookDescriptionPage bookDescriptionPage;
     ProfilePage profilePage;
     DeleteConfirmationPage deleteConfirmationPage;
+    String dialogMessage = "Book added to your collection.";
 
     @Test
     public void testAddDeleteBooksToProfile() {
@@ -43,6 +43,7 @@ public class ProfileTest extends BaseTest {
         assertThat(profilePage.getAllBooksLocator()).hasCount(amountOfBooksToAdd);
         assertThat(profilePage.getAllBooks(amountOfBooksToAdd)).containsAll(booksToAdd);
 
+        dialogMessage = "Book deleted.";
         String bookToDelete = profilePage.getBookByNumberLocator(2).textContent();
         profilePage.deleteBookByNumber(2);
         assertThat(deleteConfirmationPage.getModalTitleTextLocator()).hasText("Delete Book");
@@ -50,25 +51,18 @@ public class ProfileTest extends BaseTest {
         assertThat(deleteConfirmationPage.getOkModalButtonLocator()).hasText("OK");
         assertThat(deleteConfirmationPage.getCancelModalButtonLocator()).hasText("Cancel");
         deleteConfirmationPage.clickOkModalButton();
-        page.onceDialog(dialog -> {
-            assertEquals(dialog.message(), "Book deleted.");
-            dialog.accept();
-        });
 
         menuPage.openProfile();
         assertThat(profilePage.getAllBooksLocator()).hasCount(amountOfBooksToAdd - 1);
         assertThat(profilePage.getAllBooksLocator().allTextContents()).doesNotContain(bookToDelete);
 
+        dialogMessage = "All Books deleted.";
         profilePage.clickDeleteAllBooks();
         assertThat(deleteConfirmationPage.getModalTitleTextLocator()).hasText("Delete All Books");
         assertThat(deleteConfirmationPage.getModalBodyTextLocator()).hasText("Do you want to delete all books?");
         assertThat(deleteConfirmationPage.getOkModalButtonLocator()).hasText("OK");
         assertThat(deleteConfirmationPage.getCancelModalButtonLocator()).hasText("Cancel");
         deleteConfirmationPage.clickOkModalButton();
-        page.onceDialog(dialog -> {
-            assertEquals(dialog.message(), "All Books deleted.");
-            dialog.accept();
-        });
         assertThat(profilePage.getAllBooksLocator()).hasCount(0);
     }
 
@@ -76,15 +70,15 @@ public class ProfileTest extends BaseTest {
         Set<Book> booksToAdd = new HashSet<>();
         int randomNumberOfBook;
         int totalAmountOfBooks = bookStorePage.getAllBooksTitlesLocator().count();
+        page.onDialog(dialog -> {
+            assertEquals(dialog.message(), dialogMessage);
+            dialog.accept();
+        });
         for (int i = 1; i <= amountOfBooksToAdd; i++) {
             randomNumberOfBook = RandomGenerator.generateInt(1, totalAmountOfBooks);
             if (booksToAdd.add(bookStorePage.getBookByNumber(randomNumberOfBook))) {
                 bookStorePage.chooseBookByNumber(randomNumberOfBook);
                 bookDescriptionPage.addBookToCollection();
-                page.onceDialog(dialog -> {
-                    assertEquals(dialog.message(), "Book added to your collection.");
-                    dialog.accept();
-                });
                 bookDescriptionPage.goBackToBookStore();
             } else {
                 i--;
